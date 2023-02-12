08e0220bafd4022334d693f8d6b42e5fd8e5e4c8
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2023-02-12 22:59:17 +0300

    chnage host

--- index.js
+++ index.js
@@ -31,8 +31,6 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/
 import { initializeApp } from "firebase/app";
 import authOwner from "./middleware/authOwner.js";
 import resultApp from "./api/result_api.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
