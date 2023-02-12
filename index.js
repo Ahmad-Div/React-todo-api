51ae337da61bab4a1fb993a5cbb915cb5808bd44
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2023-02-12 22:29:57 +0300

    chnage host

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: process.env.REACT_LOCAL_HOST,
+    origin: process.env.REACT_HOST,
     optionsSuccessStatus: 200,
   })
 );
